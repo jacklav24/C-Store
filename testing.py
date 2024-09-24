87a326e20c2ca80d82cfde3c1d6b96f903d1387f
jacklav24 <jacklavergne23@gmail.com>
2024-09-24 17:33:10 -0500

    Updates

--- testing.py
+++ testing.py
@@ -3,8 +3,9 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
-constructor_standings = pd.read_csv('./c_store.csv')
+c_store_prices = pd.read_csv('./c_store.csv')
 
-sns.swarmplot(x="Item", y="Price", data=constructor_standings);
+prices = c_store_prices[c_st]
+sns.scatterplot(x="Item", y="Price", data=c_store_prices)
 
 plt.show()
